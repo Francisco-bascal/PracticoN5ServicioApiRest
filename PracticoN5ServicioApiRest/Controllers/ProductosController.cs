bf1c4ed07e2a3836ad0fc198323f6d98b0782f05
Bascal Francisco <franbascal@gmail.com>
2026-09-04 18:29:56 -0300

    Creacion de controladores y servicios de Productos (inicio)

---
+++ PracticoN5ServicioApiRest/Controllers/ProductosController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PracticoN5ServicioApiRest.Data;
+using PracticoN5ServicioApiRest.Services;
+
+namespace PracticoN5ServicioApiRest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductosController : ControllerBase
+    {
+        private readonly ProductosService _servicio;
+        public ProductosController(ProductosService servicio)
+        {
+            _servicio = servicio;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Index() 
+        {
+            return Ok(await _servicio.GetProductosAsync());
+        }
+
+
+    }
+}
---
+++ PracticoN5ServicioApiRest/Services/ProductosService.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PracticoN5ServicioApiRest.Data;
+using PracticoN5ServicioApiRest.Models;
+
+namespace PracticoN5ServicioApiRest.Services
+{
+    public class ProductosService
+    {
+        private readonly SistemaVentasDbContext _contexto;
+        public ProductosService(SistemaVentasDbContext contexto)
+        {
+            _contexto = contexto;
+        }
+
+        public async Task<ICollection<Producto>> GetProductosAsync() 
+        {
+            return(await _contexto.Productos.ToListAsync());
+        }
+
+
+    }
+}
